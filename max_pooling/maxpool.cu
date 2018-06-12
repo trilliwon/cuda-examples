55a7c293e0325c21c873ca346722538f9cbfd2ce
Won <trilliwon@gmail.com>
2018-06-12 20:28:06 +0900

    Tue Jun 12 20:28:06 KST 2018

--- max_pooling/maxpool.cu
+++ max_pooling/maxpool.cu
@@ -22,11 +22,14 @@ __global__ void maxpool(float *input, float *output, const int input_size, const
     // TODO: out of bound
     // 2D to 1D : (row, col) -> (row * N) + col
     float max_val = input[((row * filter_size) * input_size) + (col * filter_size)];
-    int counter = 0;
+    __shared__ int counter = 0;
     for (int i = row * filter_size; i < row * filter_size + filter_size; i++) {
         for (int j = col * filter_size; j < col * filter_size + filter_size; j++) {
-            max_val = fmaxf(max_val, input[(i * input_size) + j]);
-            counter ++;
+            int index = (i * input_size) + j;
+            if (index < input_size * input_size) {
+                max_val = fmaxf(max_val, input[]);
+            }
+            atomicAdd(counter, 1);
         }
     }
     printf("filter size : %d, max_value: %.1f\n", counter, max_val);
