7432004c4b070abaee5706876ee700e7b20e4c65
Won <trilliwon@gmail.com>
2018-06-12 19:07:59 +0900

    Tue Jun 12 19:07:59 KST 2018

--- max_pooling/maxpool.cu
+++ max_pooling/maxpool.cu
@@ -27,6 +27,7 @@ __global__ void maxpool(float *input, float *output, const int input_size, const
         printf("%.1f ", input[k]);
     }
     printf("%d");
+    __syncthreads(); 
     float max_val = input[index];
 
     for (int i = row * filter_size; i < filter_size; i++) {
